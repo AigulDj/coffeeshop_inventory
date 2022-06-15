fac7dd9190d911a2366ed5dec890e4fee9801227
Aigul <you@example.com>
2022-06-15 13:54:57 +0100

    Fix path of the directory on repo

--- inventory/views.py
+++ inventory/views.py
@@ -17,12 +17,7 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def update(request):
     if request.method == "POST":
-        '''
-        pass the path of the diectory where your project will be 
-        stored on PythonAnywhere in the git.Repo() as parameter.
-        Here the name of my directory is "test.pythonanywhere.com"
-        '''
-        repo = git.Repo("test.eu.pythonanywhere.com/") 
+        repo = git.Repo("AigulDj.eu.pythonanywhere.com/") 
         origin = repo.remotes.origin
 
         origin.pull()
